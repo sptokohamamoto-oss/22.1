a12d37c424ad43f9f8ca67e50700881bc39d2b67
冬桜子 濱元 <toko0124615@icloud.com>
2026-01-16 18:58:42 +0900

    a

--- ConsoleApp22/Program.cs
+++ ConsoleApp22/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+
+class Report 
+{
+    public string EmployeeName;
+    public int HoursWorked;
+
+    public Report(string employeeName, int hoursWorked)
+    {
+        EmployeeName = employeeName;
+        HoursWorked = hoursWorked;
+    }
+}
+class ReportCalculator
+{
+    public int CalculateWage(Report report) 
+    {
+        const int hourlyWage = 1000;
+        return report.HoursWorked * hourlyWage;
+    }
+}
+class ReportSaver 
+{
+    public void Save(Report Report) 
+    {
+        Console.WriteLine($"保存しました: {Report.EmployeeName},勤務時間: {Report.HoursWorked}");
+    }
+}
